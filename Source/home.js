007ca09ae41c6d91d9a57b1d9d79b52cd7bd643d
jdlars <jlarson.nwciowa@gmail.com>
2015-09-24 15:59:47 -0500

    Lab 4 Submission

--- Source/home.js
+++ Source/home.js
@@ -1,5 +1,7 @@
 var myIntroApp = angular.module('myIntroApp', ['ngRoute']);
 
+
+
 myIntroApp.config(function ($routeProvider) {
     $routeProvider
         .when('/home', {
@@ -14,6 +16,14 @@ myIntroApp.config(function ($routeProvider) {
             templateUrl: 'mapweather.html',
             controller: 'mapWeatherController'
         })
+        .when('/nav', {
+            templateUrl: 'nav.html',
+            controller: 'navigationController'
+        })
+        .when('/mashup', {
+            templateUrl: 'mashup.html',
+            controller: 'mashupController'
+        })
         .otherwise({
             redirectTo: '/home'
         });
@@ -26,8 +36,8 @@ myIntroApp.controller('mainController', function ($scope, $location) {
         $location.path(path);
     };
     $scope.login = function (username, password) {
-        localStorage.setItem(username, password);
-        $scope.go('/mapweather');
+        localStorage.setItem("currentuser", username);
+        $scope.go('/nav');
     };
 });
 
@@ -39,7 +49,63 @@ myIntroApp.controller('registerController', function ($scope, $location) {
     };
 });
 
-myIntroApp.controller('mapWeatherController', function ($scope, $http, $location) {
+myIntroApp.controller('navigationController', function ($scope, $http, $location) {
+    $scope.pageClass = 'nav';
+    $scope.go = function(path) {
+        $location.path(path);
+    };
+});
+
+myIntroApp.controller('mashupController', function ($scope, $sce, $http, $location) {
+
+    $scope.pageClass = 'mashup';
+    $scope.go = function(path) {
+        $location.path(path);
+    };
+
+    var init = function () {
+        $scope.getBeerInfo();
+    };
+
+    $scope.getBeerInfo = function () {
+        var beerUrl = "http://api.brewerydb.com/v2/brewery/avMkil/beers?withBreweries=Y&key=a309a2bb50885577a0bb30ec5a049289";
+        $http.get(beerUrl).success(function (data) {
+
+            data = data.data[0];
+            name = data.nameDisplay;
+            description = data.description;
+            icon = data.labels.large;
+            style = data.style.category.name;
+            $scope.beerHeader = $sce.trustAsHtml(name + ", a " + style);
+            $scope.beerDesc = $sce.trustAsHtml(description);
+            $scope.currentBeerIcon = $sce.trustAsHtml("<img src='" + icon + "'/>");
+            glass = data.glass.name;
+            $scope.glassDirections = $sce.trustAsHtml("You should drink this beer out of a " + glass + " glass");
+            glassIconUrl = $scope.getImageInfo(glass+"glass");
+            $scope.currentBeerGlassIcon = $sce.trustAsHtml("<img src='" + glassIcon + "'/>");
+        });
+    };
+
+    $scope.getImageInfo = function (searchString) {
+        var bingKey = "73CaJawMz9TASHEb2qA3PPVMgOKwWliCzOf3vHIJ83o";
+        var bingSecret = "a1b8d824-d982-4590-8ddd-20d7685f1712";
+        //var pintGlassSearchUrl = "https://www.googleapis.com/customsearch/v1?" +
+        //    "key=AIzaSyB0QzRNd0TQNbEfj43nRPHy9AugGiN_www&cx=011770114445841947957:fopd-9dvqks&q=" + searchString;
+        //var pintGlassSearchUrl = "https://api.datamarket.azure.com/Bing/Search/v1/Composite?" +
+        //    "AppId=" + bingKey +
+        //    "&Sources=%27image%27&Query=%27pint%20glass%27";
+        var pintGlassSearchUrl = "http://api.bing.net/json.aspx?AppId=" + bingKey +
+            "&Query=" + searchString + "%20site:microsoft.com&Sources=Image&Version=2.0" +
+            "&Market=en-us&Adult=Moderate&Image.Count=10&Image.Offset=0&JsonType=callback&JsonCallback=SearchCompleted"
+        $http.get(pintGlassSearchUrl).success(function (data) {
+            console.log(data);
+        });
+    };
+
+    init();
+});
+
+myIntroApp.controller('mapWeatherController', function ($scope, $http, $location, $sce) {
     $scope.pageClass = 'mapweather';
     $scope.go = function(path) {
         $location.path(path);
@@ -109,8 +175,8 @@ myIntroApp.controller('mapWeatherController', function ($scope, $http, $location
             icon = data.current_observation.icon_url;
             weather = data.current_observation.weather;
             console.log(temp);
-            $scope.currentweather = {html:"Currently " +temp +" &deg; F and " + weather + " in "};
-            $scope.currentIcon=  {html:"<img src='" + icon  +"'/>"};
+            $scope.currentWeather = $sce.trustAsHtml("Currently " +temp +" &deg; F and " + weather + " in " + fullCity);
+            $scope.currentIcon=  $sce.trustAsHtml("<img src='" + icon  +"'/>");
 
         });
     };
